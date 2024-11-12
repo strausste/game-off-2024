92b69d6c212e59c8e50eedcc833e91d97b7a2fa8
Luca <67017256+realSpipS@users.noreply.github.com>
2024-11-12 20:52:24 +0100

    Ui chest interazione
    non funzionante

--- Assets/Scripts/Interactables/Chast.cs
+++ Assets/Scripts/Interactables/Chast.cs
@@ -5,9 +5,39 @@ public class Chest : MonoBehaviour, IInteractable
     [SerializeField] Animator animator;
     private bool open = false;
 
+    public GameObject canvas;               
+    public float detectionRadius = 3f;     
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        canvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Direzione dello SphereCast (in questo caso, verso il basso) ??
+        Vector3 direction = Vector3.down;
+
+        if (Physics.SphereCast(transform.position, detectionRadius, direction, out RaycastHit hit, 10f))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Player trovato");
+                canvas.SetActive(true);
+            }
+        }
+        else
+        {
+            canvas.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Disegna una sfera per visualizzare il raggio di rilevamento
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
 
     public void Interact()
