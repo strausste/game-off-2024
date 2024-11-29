51ebf57c211f9ff4a2af0dabb3662c73d0a2d1be
Kallistotele <mikicabiddu@gmail.com>
2024-11-29 01:29:05 +0100

    BugFix balloon sovrapposti

--- Assets/Scripts/SymbolSpeaker.cs
+++ Assets/Scripts/SymbolSpeaker.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 
 public class SymbolSpeaker : MonoBehaviour
 {
+    GameObject balloon;
     [SerializeField] GameObject balloonPrefab;
     public void Speak(Meaning[] customPhrase, int time = 2){
         StartCoroutine(ShowBalloon(customPhrase, time));
     }
 
     IEnumerator ShowBalloon(Meaning[] phrase, int time){
-        GameObject balloon = Instantiate(balloonPrefab);
+        if(balloon)
+            Destroy(balloon);
+            
+        balloon = Instantiate(balloonPrefab);
         BalloonText text = balloon.GetComponent<BalloonText>();
         text.SetTarget(transform);
         text.Write(phrase);
